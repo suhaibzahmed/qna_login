42ef0d691749b8e223f65a410a5b1b4af422e449
Suhaib <suhaib.z.ahmed@gmail.com>
2022-01-24 18:02:52 +0530

    complete project

--- qna/src/Question.js
+++ qna/src/Question.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ title, info }) => {
+  const [btnIcon, setBtnIcon] = useState(true);
+
+  // const changeIcon = () => {
+  //   if (btnIcon === 0) setBtnIcon(1);
+  //   else setBtnIcon(0);
+  // };
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setBtnIcon(!btnIcon)}>
+          {btnIcon === true ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </header>
+      <p>{btnIcon === true ? "" : info}</p>
+    </article>
+  );
 };
 
 export default Question;
